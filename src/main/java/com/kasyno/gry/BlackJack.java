af3420e4d331d60d6db6cd320cc1c5f6faa93836
FilipO <filipopacki213@gmail.com>
2025-05-09 15:05:00 +0200

    dodalem komenrtaz

--- src/main/java/com/kasyno/gry/BlackJack.java
+++ src/main/java/com/kasyno/gry/BlackJack.java
@@ -5,7 +5,10 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+//komentarz
 
+
+//komentarz
 public class BlackJack extends Application {
 
     @Override
